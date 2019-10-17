894bf29e0d5824e3a7bf578b11e8065ddd079546
Grzegorz Rypeść <g.rypesc@samsung.com>
2019-10-17 12:01:14 +0200

    added app template

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from model.model import Model
+from controllers.main import MainController
+from views.main import MainView
+
+
+class App(QApplication):
+    def __init__(self, sys_argv):
+        super(App, self).__init__(sys_argv)
+        self.model = Model()
+        self.main_controller = MainController(self.model)
+        self.main_view = MainView(self.model, self.main_controller)
+        self.main_view.show()
+
+
+if __name__ == '__main__':
+    app = App(sys.argv)
+    sys.exit(app.exec_())
